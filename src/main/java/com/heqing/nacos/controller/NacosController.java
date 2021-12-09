75117f4addad7af78ae9479bb4655051345d6abc
heqing <heqing@yiyaowang.com>
2021-12-09 16:52:51 +0800

    nacos注册/注销

--- src/main/java/com/heqing/nacos/controller/NacosController.java
+++ src/main/java/com/heqing/nacos/controller/NacosController.java
@@ -1,5 +1,8 @@
 package com.heqing.nacos.controller;
 
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.registry.NacosRegistration;
+import com.alibaba.nacos.api.exception.NacosException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +29,12 @@ public class NacosController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    NacosDiscoveryProperties nacosDiscoveryProperties;
+
+    @Autowired
+    NacosRegistration nacosRegistration;
+
     @GetMapping("/nacos")
     public String nacos(){
         log.info("Controller --> hello nacos");
@@ -42,5 +51,53 @@ public class NacosController {
             return sb.append(host).append(":").append(port);
         }).collect(joining(", "));
         return "nacos-client 的服务地址为：" + sb.toString();
+    }
+
+    /**
+     * 服务注册接口
+     *
+     * @return
+     */
+    @GetMapping(value = "/nacos/register")
+    public String registerInstance() {
+        String serviceName = nacosDiscoveryProperties.getService();
+        String groupName = nacosDiscoveryProperties.getGroup();
+        String clusterName = nacosDiscoveryProperties.getClusterName();
+        String ip = nacosDiscoveryProperties.getIp();
+        int port = nacosDiscoveryProperties.getPort();
+
+        log.info("register from nacos, serviceName:{}, groupName:{}, clusterName:{}, ip:{}, port:{}", serviceName, groupName, clusterName, ip, port);
+        try {
+            nacosRegistration.getNacosNamingService().registerInstance(serviceName, groupName, ip, port, clusterName);
+
+        } catch (NacosException e) {
+            log.error("register from nacos error", e);
+            return "error";
+        }
+        return "success";
+    }
+
+    /**
+     * 服务注销接口
+     *
+     * @return
+     */
+    @GetMapping(value = "/nacos/deregister")
+    public String deregisterInstance() {
+        String serviceName = nacosDiscoveryProperties.getService();
+        String groupName = nacosDiscoveryProperties.getGroup();
+        String clusterName = nacosDiscoveryProperties.getClusterName();
+        String ip = nacosDiscoveryProperties.getIp();
+        int port = nacosDiscoveryProperties.getPort();
+
+        log.info("deregister from nacos, serviceName:{}, groupName:{}, clusterName:{}, ip:{}, port:{}", serviceName, groupName, clusterName, ip, port);
+        try {
+            nacosRegistration.getNacosNamingService().deregisterInstance(serviceName, groupName, ip, port, clusterName);
+
+        } catch (NacosException e) {
+            log.error("deregister from nacos error", e);
+            return "error";
+        }
+        return "success";
     }
 }
